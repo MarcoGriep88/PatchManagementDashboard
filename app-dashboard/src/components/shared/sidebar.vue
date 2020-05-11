fa133549c5d39bfca1437540addec04926f9294b
Griep <Griep.Marco@BB.INT>
2020-05-11 13:28:00 +0200

    Routing

--- app-dashboard/src/components/shared/sidebar.vue
+++ app-dashboard/src/components/shared/sidebar.vue
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
@@ -14,9 +14,27 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="/">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Patch Management</span></a>
+      </li>
+
+      <li class="nav-item active">
+        <a class="nav-link" href="/license">
+          <i class="far fa-id-badge"></i>
+          <span>License Dashboard</span></a>
+      </li>
+
+      <li class="nav-item active">
+        <a class="nav-link" href="/backup">
+          <i class="far fa-hdd"></i>
+          <span>Managed Backup</span></a>
+      </li>
+
+      <li class="nav-item active">
+        <a class="nav-link" href="/about">
+          <i class="fas fa-info-circle"></i>
+          <span>Info</span></a>
       </li>
 
       <!-- Divider -->
