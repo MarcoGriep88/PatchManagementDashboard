6fd740ce26e800cab084c284001718537ffc2766
Griep <Griep.Marco@BB.INT>
2020-06-17 13:58:42 +0200

    rebuild und Dockerfile

---
+++ app-dashboard/rebuild.sh
@@ -0,0 +1,5 @@
+docker stop app-pm-dashboard-vuejs-001
+docker rm app-pm-dashboard-vuejs-001
+npm run build
+docker build -t app-pm-dashboard-vuejs .
+docker run -dit --name app-pm-dashboard-vuejs-001 -p 80:80 app-pm-dashboard-vuejs
